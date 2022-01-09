c294dfdeb7974f97bb59857594a5a0b82da8a2b6
sebrown0 <thebrownfamily@live.co.uk>
2022-01-09 12:19:49 +0100

    Read lines from class file.

--- src/test/java/tests/FileMapperTests.java
+++ src/test/java/tests/FileMapperTests.java
@@ -12,8 +12,11 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
 import org.junit.jupiter.api.Test;
 
@@ -34,13 +37,13 @@ class FileMapperTests {
 	void file_does_not_exist() {		
 		assertFalse(FileFinder.fileExists("./src/test/java/tests", "X_FileMapperTests.java"));
 	}
-	@Test
-	void jjkjkjkjkjk() throws IOException {
-		FileReader<FileMapperTests> reader = new FileReader<FileMapperTests>(FileMapperTests.class);
-//		Class<FileMapperTests> c = FileMapperTests.class;
-		String s = reader.getInputStreamFromResources("/fileTest.txt").readFromInputStream();
-		System.out.print(s);
-	}
+//	@Test
+//	void jjkjkjkjkjk() throws IOException {
+//		FileReader<FileMapperTests> reader = new FileReader<FileMapperTests>(FileMapperTests.class);
+////		Class<FileMapperTests> c = FileMapperTests.class;
+//		String s = reader.getInputStreamFromResources("/fileTest.txt").readFromInputStream();
+//		System.out.print(s);
+//	}
 	@Test
 	public void givenFilePath_whenUsingFilesLines_thenFileData() throws URISyntaxException, IOException {
 	    String expectedData = "Hello, world!";
@@ -49,10 +52,11 @@ class FileMapperTests {
 //	    Path path = Paths.get(getClass().getClassLoader()
 //	      .getResource("fileTest.txt").toURI());
 	         
-	    Stream<String> lines = Files.lines(path);
-	    String data = lines.collect(Collectors.joining("\n"));
-	    lines.close();
+	    List<String> lines = Files.lines(path).collect(Collectors.toList());
+	    lines.forEach(ln -> System.out.println(ln));
+//	    String data = lines.collect(Collectors.joining("\n"));
+//	    lines.close();
 	         
-	    assertEquals(expectedData, data.trim());
+//	    assertEquals(expectedData, data.trim());
 	}
 }
