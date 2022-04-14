fcd41f5eb81238bd4c7a71a90552ddc147e21859
Steve Brown <sbrown@dakarsoftware.com>
2022-04-14 16:30:45 +0200

    Deleted NavBarPayrollElements.

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -14,12 +14,7 @@ import site_mapper.jaxb.pom.PomMapperApp;
  * @since 1.0
  */
 public final class Application {
-//	private static final String ROOT = 			
-//			"C:/Users/SteveBrown/eclipse-workspace/2021/DTest";
-//	
-//	private static final String XML_SOURCE = 
-//			ROOT + "/src/main/resources/site_map/site_map.xml";
-		
+	
 	private static final String ROOT = 			
 			"C:/Users/SteveBrown/eclipse-workspace/2021/AUT_DECOUPLE/src/main/java";
 	
